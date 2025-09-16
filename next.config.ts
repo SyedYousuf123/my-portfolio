3cf5c83d7bd25abf2f6856fca76105291afab796
Yousuf Ahmed <yousuf@renexusgroup.internal>
2025-09-17 00:16:25 +0500

    Running Perfectly

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
@@ -6,12 +5,3 @@ const nextConfig: NextConfig = {};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
-=======
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-}
-
-export default nextConfig;
->>>>>>> d234ea3f521a3ebfc431b19937615cc5c5fe5168
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,8 +2,4 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
-<<<<<<< HEAD
 export default config;
-=======
-export default config;
->>>>>>> d234ea3f521a3ebfc431b19937615cc5c5fe5168
